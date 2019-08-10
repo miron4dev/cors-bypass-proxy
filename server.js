4b90607d48c9e1b42b3c602ecaa14fec63568d8f
miron4dev <miron4dev@gmail.com>
2019-08-10 22:29:41 +0300

    Extend originWhitelist

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 
 const cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
+    originWhitelist: ["localhost:3001"], // Allow all origins
 }).listen(port, host, function () {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
