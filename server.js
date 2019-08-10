29a090fcc1166cb0218e5ab838ee2b1819e71870
miron4dev <miron4dev@gmail.com>
2019-08-10 22:11:39 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+// Listen on a specific host via the HOST environment variable
+const host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+const port = process.env.PORT || 5000;
+
+const cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+}).listen(port, host, function () {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
