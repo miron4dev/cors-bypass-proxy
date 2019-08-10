d8ef53f9d618ea13439de6ef5654355975cb4452
miron4dev <miron4dev@gmail.com>
2019-08-10 22:36:43 +0300

    cors-proxy.js has been added

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
+import * as proxy from './cors-proxy';
+
 // Listen on a specific host via the HOST environment variable
 const host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
 const port = process.env.PORT || 5000;
 
-const cors_proxy = require('cors-anywhere');
-cors_proxy.createServer({
-    originWhitelist: ["localhost:3001"], // Allow all origins
+proxy.createServer({
+    originWhitelist: [], // Allow all origins
 }).listen(port, host, function () {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
